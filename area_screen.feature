c87736db2ce63cebdc625efd76210c18d5ef89fa
trrigger <alena.kirillova@e-legion.com>
2018-02-03 14:56:47 +0300

    Cornerstone
    Project start: features and steps definitions, hooks, env, apk.

---
+++ area_screen.feature
@@ -0,0 +1,25 @@
+Feature: User is able to convert area units
+  Scenario: User is able to dismiss help
+    Given I land on help popup
+    When I click on Got it button
+    Then I land on Area screen
+
+  Scenario: User is able to swap values
+    Given I click on Got it button
+    And I see "Sq Kilometre" in From header
+    And I see "Sq Metre" in To header
+    When I click on Swap button
+    Then I see "Sq Metre" in From header
+    And I see "Sq Kilometre" in To header
+
+  @wip
+  Scenario Outline: User is able to convert default units
+    Given I click on Got it button
+    And I click on Clear button
+    When I enter "<target>" to From field
+    Then I get "<result>" in To field
+    Examples:
+      |target|result|
+      |1     |1000000|
+      |2     |2000000|
+      |3     |3000000|
---
+++ step_definitions/area screen steps.rb
@@ -0,0 +1,35 @@
+Given(/^I land on help popup$/) do
+  puts("User lands on Help popup")
+end
+
+When(/^I click on Got it button$/) do
+  puts ("User clicks on Got it button")
+end
+
+Then(/^I land on Area screen$/) do
+  puts ("User lands on area screen")
+end
+
+When(/^I click on Swap button$/) do
+  puts("User clicks Swap button")
+end
+
+Then(/^I see "([^"]*)" in From header$/) do |value|
+  puts("From header values is #{value}")
+end
+
+And(/^I see "([^"]*)" in To header$/) do |value|
+  puts("To header values is #{value}")
+end
+
+And(/^I click on Clear button$/) do
+  puts("User clicks on Clear button")
+end
+
+When(/^I enter "([^"]*)" to From field$/) do |value|
+  puts("User entered value #{value}")
+end
+
+Then(/^I get "([^"]*)" in To field$/) do |value|
+  puts("User sees #{value} in result field")
+end
