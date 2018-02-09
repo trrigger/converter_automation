18c4a0cbc36baacfe064488204f34fe4da13d070
trrigger <alena.kirillova@e-legion.com>
2018-02-09 23:41:26 +0300

    Test with scroll added

--- features/step_definitions/volume_screen_steps.rb
+++ features/step_definitions/volume_screen_steps.rb
@@ -8,3 +8,15 @@ Then(/^I see app menu$/) do
   find_element(id: "design_navigation_view").find_element(xpath: "//android.widget.CheckedTextView[@text='Cooking']")
   find_element(id: "design_navigation_view").find_element(xpath: "//android.widget.CheckedTextView[@text='Currency']")
 end
+
+And(/^I select "([^"]*)" from menu$/) do |value|
+
+  until exists {find_element(id: "design_navigation_view").find_element(xpath: "//android.widget.CheckedTextView[@text='#{value}']")} do
+    Appium::TouchAction.new.swipe(start_x: 0.5, start_y: 0.8, end_x: 0.5, end_y: 0.2, duration: 500).perform
+  end
+  find_element(id: "design_navigation_view").find_element(xpath: "//android.widget.CheckedTextView[@text='#{value}']").click
+end
+
+Then(/^I land on "([^"]*)" screen$/) do |value|
+  find_element(id: "toolbar").find_element(xpath: "//android.widget.TextView[@text='#{value}']")
+end
--- features/volume_screen.feature
+++ features/volume_screen.feature
@@ -4,7 +4,18 @@ Feature: User is able to convert Volume units
     Given I click on Got it button
     And I land on Area screen
 
-  @wip
   Scenario: User is able to call menu by swiping
     When I swipe in the menu
     Then I see app menu
+
+  @wip
+  Scenario Outline:
+    When I swipe in the menu
+    And I select "<target>" from menu
+    Then I land on "<result>" screen
+
+    Examples:
+    |target|result|
+    |Power |Power |
+    |Volume|Volume|
+    |Currency|Currency|
